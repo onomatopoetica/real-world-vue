b1078151e418801518a36883a9ba1306678ef076
Jenn Bates <jenn.bates@nikolamotor.com>
2021-09-02 15:50:07 -0700

    Component anatomy, Nsted components, scoped styles

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,15 +1,55 @@
 <template>
-  <h1 v-if="test" @click="handler(arg, event)"></h1>
+  <router-link
+    class="event-link"
+    :to="{ name: 'Event-Show', params: { id: '1' } }"
+  >
+    <div class="event-card -shadow">
+      <span class="eyebrow">@{{ event.time }} on {{ event.date }}</span>
+      <h4 class="title">{{ event.title }}</h4>
+      <span>{{ event.attendees.length }} attending</span>
+    </div>
+  </router-link>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      quote: 'I want to be single',
+      event: {
+        id: 1,
+        title: 'Beach Cleanup',
+        date: 'Tues Sept 21, 2021',
+        time: '6:00',
+        attendees: [
+          { id: '0001', name: 'Rouxbe Doux' },
+          { id: '0002', name: 'Scarlett Roux' },
+          { id: '0003', name: 'Me Too' },
+        ],
+      },
     }
   },
 }
 </script>
 
-<style></style>
+<style scoped>
+.event-card {
+  padding: 20px;
+  margin-left: 0px;
+  margin-bottom: 24px;
+  transition: all 0.2s linear;
+  cursor: pointer;
+}
+.event-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2), 0 1px 15px 0 rgba(0, 0, 0, 0.19);
+}
+.event-card > .title {
+  margin: 0;
+}
+
+.event-link {
+  color: black;
+  text-decoration: none;
+  font-weight: 100;
+}
+</style>
