040ca246e3b086bc64d5ca89c58fcf5c5962050f
Jenn Bates <jenn.bates@nikolamotor.com>
2021-09-07 10:59:41 -0700

    Added datepicker + getters

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,5 +1,27 @@
 <template>
   <div class="EventCreate">
-    <h1>Create Event!</h1>
+    <h1>Create an Event, {{ user.name }}</h1>
+    <p>This event was created by {{ user.name }}</p>
+    <!-- <p>There are {{ catLength }} categories</p> -->
+    <p>{{ getEvent(2) }}</p>
   </div>
 </template>
+
+<script>
+import { mapState, mapGetters } from 'vuex'
+
+export default {
+  computed: {
+    // catLength() {
+    //   return this.$store.getters.catLength
+    // },
+    getEvent() {
+      return this.$store.getters.getEventById
+    },
+    ...mapState(['user', 'categories']),
+    ...mapGetters(['getEventById']),
+  },
+}
+</script>
+
+<style></style>
