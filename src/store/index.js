16fd2c86390378743dcec3282795a9b1b177cbab
Jenn Bates <jenn.bates@nikolamotor.com>
2021-09-07 15:28:07 -0700

    Fetch events w Vuex + Pagination

--- src/store/index.js
+++ src/store/index.js
@@ -6,7 +6,7 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    user: { id: '0003', name: 'Scarlett Roux' },
+    user: { id: 'abc123', name: 'Adam Jahr' },
     categories: [
       'sustainability',
       'nature',
@@ -17,10 +17,21 @@ export default new Vuex.Store({
       'community',
     ],
     events: [],
+    eventsTotal: 0,
+    event: {},
   },
   mutations: {
     ADD_EVENT(state, event) {
       state.events.push(event)
+    },
+    SET_EVENTS(state, events) {
+      state.events = events
+    },
+    SET_EVENTS_TOTAL(state, eventsTotal) {
+      state.eventsTotal = eventsTotal
+    },
+    SET_EVENT(state, event) {
+      state.event = event
     },
   },
   actions: {
@@ -28,6 +39,33 @@ export default new Vuex.Store({
       return EventService.postEvent(event).then(() => {
         commit('ADD_EVENT', event)
       })
+    },
+    fetchEvents({ commit }, { perPage, page }) {
+      EventService.getEvents(perPage, page)
+        .then((response) => {
+          commit(
+            'SET_EVENTS_TOTAL',
+            parseInt(response.headers['x-total-count'])
+          )
+          commit('SET_EVENTS', response.data)
+        })
+        .catch((error) => {
+          console.log('There was an error:', error.response)
+        })
+    },
+    fetchEvent({ commit, getters }, id) {
+      let event = getters.getEventById(id)
+      if (event) {
+        commit('SET_EVENT', event)
+      } else {
+        EventService.getEvent(id)
+          .then((response) => {
+            commit('SET_EVENT', response.data)
+          })
+          .catch((error) => {
+            console.log('There was an error:', error.response)
+          })
+      }
     },
   },
   getters: {
